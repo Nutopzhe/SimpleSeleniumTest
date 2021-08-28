57db2abcf80041d4f359a46f4b1fcc8c11800378
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-28 21:47:08 +0300

    Refactoring

--- src/test/java/ru/rybakov/HowToDoInJavaRegressTest.java
+++ src/test/java/ru/rybakov/HowToDoInJavaRegressTest.java
@@ -1,19 +1,14 @@
 package ru.rybakov;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
